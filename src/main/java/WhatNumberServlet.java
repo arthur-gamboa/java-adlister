52dfcfbd0cff16b561b9ea26922c319da1205da0
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-03-04 10:16:44 -0600

    adding user number for example

--- src/main/java/WhatNumberServlet.java
+++ src/main/java/WhatNumberServlet.java
@@ -3,14 +3,16 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-@WebServlet(name = "WhatNumberServlet", urlPatterns = "what-num")
+@WebServlet(name = "WhatNumberServlet", urlPatterns = "/what-num")
 public class WhatNumberServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String userNum = req.getParameter("num");
+
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        out.println("Let's play a game!");
+        out.println("<h1>User Number was: " + userNum + "</h1>");
     }
 
 }
